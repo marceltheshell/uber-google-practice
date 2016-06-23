84e45693eaefa604be74206ec7cbb05abc7b909d
Marcel Degas <marcel.degas@gmail.com>
2016-06-22 20:30:48 -0700

    added a jquery method to grab the values from the front end

--- public/app.js
+++ public/app.js
@@ -1,1 +1,18 @@
-app.js
+$('document').ready(function() {
+
+	$('form.findPrice').on('submit',function(e){
+
+		e.preventDefault();
+		address = $(this).serializeArray();
+		origin = address[0].value;
+		destination = address[1].value;
+
+		console.log(origin)
+
+		// mapCoordinates(origin, destination);
+
+		// $('.findPrice').trigger('reset');
+
+	});
+
+})
