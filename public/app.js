609dd01590aa9f37e50483e8d58ea381f27d8b0b
Marcel Degas <marcel.degas@gmail.com>
2016-06-23 22:52:46 -0700

    cleaned up app.js a bit

--- public/app.js
+++ public/app.js
@@ -14,7 +14,7 @@ markerArray = [];
 
 
 $('document').ready(function() {
-
+    initAutocomplete();
     $('form.findPrice').on('submit',function(e){
         e.preventDefault();
         address = $(this).serializeArray();
@@ -25,12 +25,10 @@ $('document').ready(function() {
 })
 
 
-// function initAutocomplete() {
-//      autocomplete = new google.maps.places.Autocomplete((document.getElementById('start')), options);
-  
-//      autocompleteEnd = new google.maps.places.Autocomplete((document.getElementById('end')), options);
-  
-// }
+function initAutocomplete() {
+     autocomplete = new google.maps.places.Autocomplete((document.getElementById('start')), options);
+     autocompleteEnd = new google.maps.places.Autocomplete((document.getElementById('end')), options);
+}
 
 
 function mapCoordinates(addressOne, addressTwo){
@@ -50,12 +48,7 @@ function mapCoordinates(addressOne, addressTwo){
         formattedOriginLtLg = originLtLg[0].results[0].geometry.location
         formattedDestinLtLg = destinLtLg[0].results[0].geometry.location
         renderGoogleMap(formattedOriginLtLg, formattedDestinLtLg);
-
-
         getRidePrices(formattedOriginLtLg, formattedDestinLtLg);
-
-
-
 
     }).fail(function() {
         console.log("oops, something went wrong while getting directions");
@@ -64,7 +57,6 @@ function mapCoordinates(addressOne, addressTwo){
 }
 
 function renderGoogleMap(originCoord, destinCoord){
-  
     var directionsService = new google.maps.DirectionsService();
     $('#map').animate({'height':'384px'},2000, function(){    
         map = new google.maps.Map(document.getElementById("map"),{
@@ -76,14 +68,15 @@ function renderGoogleMap(originCoord, destinCoord){
               scaleControl: false,
               scrollwheel: false,
               disableDoubleClickZoom: true
+        });
+    });
 
-        })
-    })
-  
-    // /*Create a Renderer for Directions and Binds it to the Map*/
+    //this looks like it could be an issue
     var directionsDisplay = new google.maps.DirectionsRenderer({map: map}); 
     
-    /////////// CalcRoute is for Google////////////////////////
+
+
+
     calcRoute(originCoord, destinCoord)
 
     function calcRoute(startCoord, endCoord) {
@@ -100,64 +93,8 @@ function renderGoogleMap(originCoord, destinCoord){
                 directionsDisplay.setDirections(result);
             }
         });
-
     }
-
 }
-
-
-
-
-
-/*FS Displays the Route Between the Initial Start and End Selections.*/
-// calculateAndDisplayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, map);
-
-
-
-
-
-
-
-/* Calculate Display Route Function*/
-
-// function calculateAndDisplayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, map) {
-      
-//   First, remove any existing markers from the map.
-//   for (var i = 0; i < markerArray.length; i++) {
-//     markerArray[i].setMap(null);
-//   }
-
-
-
-
-  /* Retrieve the Start and End Locations and Create a DirectionsRequest Using DRIVING Directions.*/
- //  directionsService.route({
- //                            origin: origin,
- //                            destination: destination,
- //                            travelMode: google.maps.TravelMode.DRIVING
- //                          }, 
- //                          function(response, status){
-
- //                              // Route the directions and pass the response to a function to create
- //                              // markers for each step.
- //                            if (status === google.maps.DirectionsStatus.OK) {
-
- //                                  document.getElementById('warnings-panel').innerHTML = '<b>' + response.routes[0].warnings + '</b>';
-
-
- //                                  directionsDisplay.setDirections(response);
- //                                  // showSteps(response, markerArray, stepDisplay, map);
- //                              } 
- //                            else {
- //                                window.alert('Directions request failed due to ' + status);
- //                            }
- //                        });
- 
- // }
-
-
-
-// /*Request Ride Prices Function*/
 
 function getRidePrices(origin, destination){
     var coordinates = {};
@@ -165,19 +102,14 @@ function getRidePrices(origin, destination){
     coordinates.lng1 = origin.lng;
     coordinates.lat2 = destination.lat;
     coordinates.lng2 = destination.lng;
-
     $.post('/uberPrice', {coordinates: coordinates}, function(data){
         var rides = data.prices;
         $.each( rides, function( i, ride ) {
-            
-            console.log(ride);
             var newListItem = ("<tr><td>" + ride.display_name + "</td><td>" + ride.estimate + "</td><td>" + ride.distance + " miles"+ "</td></tr>");
-            // var newListItem = "<li>" + ride.display_name + "</li>";
             $( "#estimates" ).append( newListItem );
         });
 
     });
-
 };
 
 
